09b84881c000ea25369eb934710b82337b7de6d1
Brito <britodfbr@gmail.com>
2018-05-05 10:56:23 -0300

    atualizações diversas

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-#Projeto desenvolvido por incolume.com.br
+# Projeto desenvolvido por incolume.com.br
 
-## Gerar pacotes para instalação
+Este pacote disponibiliza diversas funcionalidades para utilização em rotinas com Python.
+
+Dentre elas:
+* geração de números de CPF não verificados;
+* geração de lista para namespace para pacotes Python;
+* obtenção rápida para conteúdo de arquivos;
+* sequências de vários tipos;
+* Nome único para gravação de arquivos sem sobrescrita;
+
+## Gerar pacote a partir dos fontes para instalação
+
 python setup.py bdist_egg bdist_wheel
--- incolumepy/utils/utils.py
+++ incolumepy/utils/utils.py
@@ -7,44 +7,51 @@ def read(*rnames):
     :param rnames:
     :return:
     >>> read(os.path.dirname(__file__), 'version.txt')
-    0.2
+    '0.6'
+
+    >>> read(os.path.dirname(__file__), 'README')
+    'incolumepy.utils'
+
     '''
     with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
+        return f.read().strip()
 
 
 def namespace(package_name):
     '''
-    return the namespace from to s='incolumepy.package.module'
-    :param s:
-    :return:
+    return the namespace from package_name='incolumepy.package.module' ['incolumepy','incolumepy.package']
+    :param package_name: str
+    :return: list
 
     >>> namespace('incolumepy.package.module')
-    ['incolumepy','incolumepy.package']
+    ['incolumepy', 'incolumepy.package']
 
     >>> namespace('incolumepy')
     ['incolumepy']
     '''
+    #print(package_name)
     s = package_name.split('.')
+    #print(s)
     l = []
-    if len(s)<=0:
+    if len(package_name)<=0:
         raise ValueError('package_name not can be void')
     elif len(s) == 1:
         l.append(package_name)
     else:
         for item in s[:-1]:
-            print(item)
             if l:
                 l.append('{}.{}'.format(l[-1], item))
             else:
                 l.append(item)
-            print(l)
+            #print(l)
     return l
 
+def run():
+    print(namespace('incolumepy.package.subpackage.module'))
+    print(namespace('incolumepy.package'))
+    print(namespace('incolumepy'))
+    print(namespace(''))
 
 if __name__ == "__main__":
     pass
-    #namespace('incolumepy.package.subpackage.module')
-    #namespace('incolumepy.package')
-    namespace('incolumepy')
-    namespace('')
+    #run()
