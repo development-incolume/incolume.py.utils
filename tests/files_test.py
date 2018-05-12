4f6015eb6490dd941c9f90b3d6fe607f9012d1dd
Brito <britodfbr@gmail.com>
2018-05-12 12:14:07 -0300

    testes de criação dos arquivos movidos para tests/files_test.py

--- tests/files_test.py
+++ tests/files_test.py
@@ -3,15 +3,19 @@ import sys
 import unittest
 import shutil
 from incolumepy.utils.files import realfilename
-
+from incolumepy.utils.files import ll
 
 class UtilsTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.directories = []
 
     def __del__(self, *args, **kwargs):
-        pass
-        shutil.rmtree(os.path.join(os.path.dirname(__file__), 'tmp'), ignore_errors=True)
+        #print('init destructor')
+        for dir in self.directories:
+            #print(dir)
+            shutil.rmtree(dir, ignore_errors=True)
+        #print('finish destructor')
 
     def test_files01(self):
         self.assertTrue(realfilename('version.txt'))
@@ -20,15 +24,18 @@ class UtilsTest(unittest.TestCase):
         self.assertTrue(realfilename('README'))
 
     def test_files03(self):
-        file = realfilename(os.path.join('tmp', 'britodfbr','diretorio', 'para', 'teste'),ext='.dat', separador=True)
+        self.directories.append(os.path.join('tmp', 'britodfbr','diretorio', 'para', 'teste'))
+        file = realfilename(self.directories[-1],ext='.dat', separador=True)
         self.assertEqual(sys.stdout.getvalue().strip(), 'Criado arquivo: {}'.format(file))
 
     def test_files04(self):
-        file = realfilename(os.path.join('tmp', 'diretorio', 'para', 'teste'),separador=True, ext='md')
+        self.directories.append(os.path.join('tmp', 'diretorio', 'para', 'teste'))
+        file = realfilename(self.directories[-1],separador=True, ext='md')
         self.assertEqual(sys.stdout.getvalue().strip(), 'Criado arquivo: {}'.format(file))
 
     def test_files05(self):
-        file = realfilename(('tmp/teste/test.json'), separador=True, ext='bash')
+        self.directories.append('tmp/teste/test.json')
+        file = realfilename(self.directories[-1], separador=True, ext='bash')
         self.assertEqual(sys.stdout.getvalue().strip(), 'Criado arquivo: {}'.format(file))
 
     def test_files06(self):
@@ -52,13 +59,102 @@ class UtilsTest(unittest.TestCase):
         self.assertEqual(sys.stdout.getvalue().strip(), 'Criado arquivo: {}'.format(file))
 
     def test_files11(self):
+        self.directories.append('/tmp/utils/')
         file = realfilename('/tmp/utils/tmp/registro.xml')
         self.assertEqual(sys.stdout.getvalue().strip(), 'Criado arquivo: {}'.format(file))
+
+    def test_files_realfilename01(self):
+        self.directories.append(os.path.join('tmp', 'britodfbr', 'diretorio', 'para', 'teste'))
+        with open(realfilename(self.directories[-1],
+                ext='.dat', separador=True), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename02(self):
+        self.directories.append(os.path.join('tmp', 'diretorio', 'para', 'teste'))
+        with open(realfilename(self.directories[-1],
+                separador=True, ext='md'), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename03(self):
+        self.directories.append('tmp/teste/test.json')
+        with open(realfilename(self.directories[-1],
+                               separador=True, ext='bash'), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename04(self):
+        self.directories.append('tmp/teste/lll')
+        with open(realfilename(self.directories[-1],
+                               separador=True), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename05(self):
+        self.directories.append('tmp/teste/jjj.json')
+        with open(realfilename(self.directories[-1],
+                               separador=True), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename06(self):
+        self.directories.append('tmp')
+        with open(realfilename(('tmp/teste/jjj.json'),
+                               separador=True), 'w') as file:
+            file.write('teste ok')
+
+    def test_files_realfilename07(self):
+        self.directories.append('tmp')
+        with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+                               digits=4, ext='log', separador=False), 'a') as file:
+            file.write(file.name)
+
+
+    def test_files_realfilename08(self):
+        self.directories.append('tmp')
+        with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+                               digits=4, ext='log', separador=False), 'a') as file:
+            file.write(file.name)
+
+    def test_files_realfilename09(self):
+        self.directories.append('tmp')
+        with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+                               digits=5, ext='log', separador=True), 'a') as file:
+            file.write(file.name)
+
+    def test_files_realfilename10(self):
+        self.directories.append('tmp')
+        with open(realfilename(os.path.join('tmp', os.path.basename(__file__)),
+                               digits=5, ext='csv'), 'a') as file:
+            file.write(file.name)
+
+    def test_files_realfilename11(self):
+        self.directories.append('../teste_utils/')
+        with open(realfilename('../teste_utils/tmp/registro.xml'), 'w') as file:
+            file.write(file.name)
+
+
+    def test_files_ll01(self):
+        self.directories.append('tmp/teste1')
+        os.mkdir(self.directories[-1], mode=0o777)
+
+
+    def test_files_ll02(self):
+        self.directories.append('tmp/teste1')
+        result = []
+        for i in range(3):
+            with open(realfilename(os.path.join(self.directories[-1], 'file')), 'w') as file:
+                file.write(file.name)
+
+        #print('>', ll(self.directories[-1], string=False))
+        for i, j in ll(self.directories[-1], string=False):
+            result.append(j)
+        #print('>', result)
+        self.assertTrue(result, "['file_01.txt', 'file.txt', 'file_02.txt']")
+
+    def test_files_ll03(self):
+        pass
+
 
     @staticmethod
     def main():
         super().main()
-        shutil.rmtree('tmp')
 
 
 if __name__ == '__main__':
