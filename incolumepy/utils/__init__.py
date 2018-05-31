e19433c7d92182b04cb4a0e6f64d9edf152b44e5
Brito <britodfbr@gmail.com>
2018-05-31 16:38:34 -0300

    nonexequi implementado

--- incolumepy/utils/__init__.py
+++ incolumepy/utils/__init__.py
@@ -1,3 +1,19 @@
+from functools import wraps
+
+
+def nonexequi(a_func):
+    '''
+
+    :param a_func:
+    :return:
+    '''
+    @wraps(a_func)
+    def wrapTheFunction(self):
+        return ('Skip: {}'.format(a_func.__name__))
+
+    return wrapTheFunction
+
+
 # See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
 try:
     __import__('pkg_resources').declare_namespace(__name__)
