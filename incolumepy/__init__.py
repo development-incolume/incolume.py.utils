1029fbdb8e4041eb233b327fc6269406809de43a
Brito <britodfbr@gmail.com>
2018-06-05 23:10:32 -0300

    ajustes no namespace

--- incolumepy/__init__.py
+++ incolumepy/__init__.py
@@ -4,3 +4,6 @@ try:
 except ImportError:
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__)
+
+#__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
--- incolumepy/utils/__init__.py
+++ incolumepy/utils/__init__.py
@@ -1,4 +1,7 @@
 from functools import wraps
+from os.path import abspath, join, dirname
+
+__version__ = open(abspath(join(dirname(__file__), 'version.txt'))).read().strip()
 
 
 def nonexequi(a_func):
@@ -13,10 +16,3 @@ def nonexequi(a_func):
 
     return wrap_the_function
 
-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
