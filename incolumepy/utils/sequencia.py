dd04892661c71c0ef72e925c223552972dbea3a3
Brito <britodfbr@gmail.com>
2018-06-08 23:14:59 -0300

    modulo incolumepy.utils.sequencia emancipado

--- incolumepy/utils/sequencia.py
+++ incolumepy/utils/sequencia.py
@@ -1,57 +1,73 @@
 #coding: utf-8
 from math import sqrt
+from deprecated import deprecated
 
 
+@deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
 class Sequencia:
 
     class Fibonacci:
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __init__(self):
             self.seq = [1, 1]
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __next__(self):
             self.seq.append(sum(self.seq))
             return self.seq.pop(0)
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __iter__(self):
             return Sequencia.Fibonacci()
 
     class Naturais:
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __init__(self):
             self.lista = [0]
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __next__(self):
             self.lista.append(self.lista[0] + 1)
             return self.lista.pop(0)
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __iter__(self):
             return Sequencia.Naturais()
 
 
     class Pares:
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __init__(self):
             self.lista = [2]
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __next__(self):
             self.lista.append(self.lista[0] + 2)
             return self.lista.pop(0)
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __iter__(self):
             return Sequencia.Pares()
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def ispar(self, num):
             return num % 2 == 0
 
     class Impares:
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __init__(self):
             self.lista = [1]
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __next__(self):
             self.lista.append(self.lista[0] + 2)
             return self.lista.pop(0)
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __iter__(self):
             return Sequencia.Impares()
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def isimpar(self, num):
             return num % 2 == 1
 
@@ -61,10 +77,12 @@ class Sequencia:
         '''
         primos = []
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __init__(self):
             self.primos = [2, 3, 5, 7]
             self.seq = Sequencia.Naturais()
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __next__(self):
             while 1:
                 value = self.seq.__next__()
@@ -72,9 +90,11 @@ class Sequencia:
                     self.primos.append(value)
                     return self.primos[-1]
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def __iter__(self):
             return Sequencia.Primos()
 
+        @deprecated(version='0.9.2', reason="Replaced for incolumepy.sequencias")
         def isprimo(self, numero):
 
             if numero in self.primos:
