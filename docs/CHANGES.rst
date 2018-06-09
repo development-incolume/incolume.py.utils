afcca055f0708a5c5046903d126a80068da135a5
Brito <britodfbr@gmail.com>
2018-06-09 08:11:31 -0300

    Tratativa de executar varios  modulos incolumepy na mesma aplicação

--- docs/CHANGES.rst
+++ docs/CHANGES.rst
@@ -1,3 +1,4 @@
+0.9.4           Chamada do pacote utils através do Namespace
 0.9.3           Nova implementação para incolumepy.utils.utils.namespace
 0.9.2           package incolumepy.utils.sequencias remaked into incolumepy.sequencias
 0.9.1           corrigido namespace
--- incolumepy/utils/__init__.py
+++ incolumepy/utils/__init__.py
@@ -1,8 +1,12 @@
 from functools import wraps
 from os.path import abspath, join, dirname
+from incolumepy.utils.utils import namespace, read
 
-__version__ = open(abspath(join(dirname(__file__), 'version.txt'))).read().strip()
-
+# __version__ = open(abspath(join(dirname(__file__), 'version.txt'))).read().strip()
+__version__ = read('version.txt')
+__title__ = 'incolumepy.utils'
+__namespace__ = namespace(__title__)
+__name__ = __title__.split('.')[-1]
 
 def nonexequi(a_func):
     ''' This decorator when apply over def, the def dont work, but return a message informing that skip.
