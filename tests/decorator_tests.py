6b9166ee7d5bf4e9b103b237b37705b3faf813e9
@britodfbr <britodfbr@gmail.com>
2018-09-23 19:40:56 -0300

    implementações e teste do decorador @time_it

--- tests/decorator_tests.py
+++ tests/decorator_tests.py
@@ -8,25 +8,31 @@ class DecoratorTests(TestCase):
     def setUpClass(cls):
         pass
 
-    @time_it
+
     def xpto(self):
-        ''' This ok! '''
+        '''This ok!'''
         n = 0
         for i in range(100000):
             n *= i**2
         return n
 
+    @time_it
+    def fx(self):
+        return 'x'
+
     def setUp(self):
         pass
 
     def test_name(self):
-        self.assertAlmostEqual('xpto', self.xpto.__name__)
+        self.assertEqual('xpto', self.xpto.__name__)
+        # self.assertEqual('fx', self.fx.__name__)
 
     def test_doc(self):
         self.assertEqual('This ok!', self.xpto.__doc__)
+        # self.assertEqual('This ok!', self.fx.__doc__)
 
     def test_output(self):
-        print(self.xpto())
+        ...
 
 
 if __name__ == '__main__':
