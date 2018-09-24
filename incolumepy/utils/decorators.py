9951ed7fa5041fcbb59b8367a535d5ce879394a3
@britodfbr <britodfbr@gmail.com>
2018-09-23 22:53:12 -0300

    correção no @wraps

--- incolumepy/utils/decorators.py
+++ incolumepy/utils/decorators.py
@@ -3,11 +3,11 @@ from functools import wraps
 
 
 def time_it(func):
-    @wraps
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start = time()
         result = func(*args, **kwargs)
         end = time()
-        print('{}: {5:3.2f} ms'.format(func.__name__, 1000*(end-start)))
+        print('{}: {:3.5f} ms'.format(func.__name__, 1000 * (end - start)))
         return result
     return wrapper
