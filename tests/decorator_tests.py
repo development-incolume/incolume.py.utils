edcdd065fe88d50e558272753b3e8b4af21ea19f
@britodfbr <britodfbr@gmail.com>
2018-09-23 22:54:47 -0300

    testes dundler name, doc, return e output realizados

--- tests/decorator_tests.py
+++ tests/decorator_tests.py
@@ -1,5 +1,6 @@
 from incolumepy.utils.decorators import time_it
 from unittest import TestCase, main
+import sys
 
 
 class DecoratorTests(TestCase):
@@ -8,32 +9,55 @@ class DecoratorTests(TestCase):
     def setUpClass(cls):
         pass
 
-
+    @time_it
     def xpto(self):
         '''This ok!'''
-        n = 0
+        n = 1
         for i in range(100000):
             n *= i**2
         return n
 
     @time_it
     def fx(self):
-        return 'x'
+        '''This ok!'''
+        return 'fx'
 
     def setUp(self):
         pass
 
     def test_name(self):
         self.assertEqual('xpto', self.xpto.__name__)
-        # self.assertEqual('fx', self.fx.__name__)
+        self.assertEqual('fx', self.fx.__name__)
+
 
     def test_doc(self):
         self.assertEqual('This ok!', self.xpto.__doc__)
-        # self.assertEqual('This ok!', self.fx.__doc__)
+        self.assertEqual('This ok!', self.fx.__doc__)
+
+    def test_return(self):
+        self.assertEqual(0, self.xpto())
+        self.assertEqual('fx', self.fx())
 
     def test_output(self):
-        ...
+        self.xpto()
+        output = sys.stdout.getvalue().strip()
+        self.assertEqual('', output)
 
+    def test_output_mock0(self):
+        from io import StringIO
+        from unittest.mock import patch
+
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            self.xpto()
+            self.assertRegex(fakeOutput.getvalue().strip(), '^xpto: \d*.?\d+ ms$')
+
+    def test_output_mock1(self):
+        from io import StringIO
+        from unittest.mock import patch
+
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            self.fx()
+            self.assertRegex(fakeOutput.getvalue().strip(), '^fx: \d*.?\d+ ms$')
 
 if __name__ == '__main__':
-    main
+    main()
