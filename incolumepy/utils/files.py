020a77cd484ad117ded8d86fb600626aaeab1fd6
Britodfbr <britodfbr@gmail.com>
2022-01-23 14:28:48 -0300

    chore: #2 Desativado cobertura para método obsoleto

--- incolumepy/utils/files.py
+++ incolumepy/utils/files.py
@@ -6,9 +6,11 @@ from deprecated import deprecated
 import logging
 import os
 
+__author__ = "@britodfbr"
+
 
 @deprecated(version="1.6.0", reason="Replaced for pathlib.Path.glob or pathlib.Path.rglob")
-def ll(path=".", ext=None, string=True, recursive=False):
+def ll(path=".", ext=None, string=True, recursive=False):  # pragma: no cover
     """Recursive or single list of file on directory.
 
     recursive=True return list recursive, string=True return list of string(path+file),
