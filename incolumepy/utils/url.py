5a22567ebc29fcd78214f9465f0d62f7715055ae
Britodfbr <britodfbr@gmail.com>
2022-02-16 14:09:59 -0300

    feat: #31 Incorporado nova funcionalidade

--- incolumepy/utils/url.py
+++ incolumepy/utils/url.py
@@ -40,3 +40,40 @@ def check_url(url: str) -> bool:
     )
 
     return True if re.match(regex, url) else False
+
+
+def identify_dom_url(url: str, lista_dominio: list = None, verboso: bool = False) -> str:
+    """
+    Locate match standard on url by lista_dominio, ideal for use into pandas.
+
+    :param url: referida url
+    :param verboso: verbosity mode
+    :param lista_dominio: lista com os dominios default ['planalto', 'camara', 'senado']
+    :return: dominio
+
+    >>> identify_dom_url(
+    'https://www2.camara.leg.br/legin/fed/carreg_sn/anterioresa1824/\
+    cartaregia-39331-10-julho-1818-569289-publicacaooriginal-92518-pe.html'
+    )
+    'camara'
+    >>> identify_dom_url('https://www.planalto.gov.br/ccivil_03/leis/lim/lim-26-8-1826.htm')
+    'planalto'
+    >>> identify_dom_url('http://legis.senado.leg.br/norma/416863/publicacao/15637291')
+    'senado'
+    >>> identify_dom_url('https://www.google.com.br')
+
+    >>> identify_dom_url('https://www.google.com.br', ['google'])
+    'google'
+    >>> identify_dom_url('https://google.com', verboso=True)
+    args: (url='https://google.com', lista_dominio=['planalto', 'camara', 'senado'])
+    >>> identify_dom_url('https://google.com', ['google'], True)
+    args: (url='https://google.com', lista_dominio=['google'])
+    'google'
+    """
+    lista_dominio = lista_dominio or ["planalto", "camara", "senado"]
+    if verboso:
+        print(f"args: (url: {url}, lista_dominio: {lista_dominio})")
+    for dominio in lista_dominio:
+        if re.compile(dominio).search(url):
+            return dominio
+    return ""
