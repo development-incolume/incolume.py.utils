bccfae714e2714f20f487eb1eed65c2343551f79
Britodfbr <britodfbr@gmail.com>
2022-03-07 08:34:02 -0300

    test: #36 Teste para aferir Versionamento Semantico concluído
    close #36

--- tests/test_incolumepy_utils.py
+++ tests/test_incolumepy_utils.py
@@ -27,6 +27,36 @@ def test_file_exist(entrance):
 
 
 @pytest.mark.parametrize(
+    ["entrance", "expected"],
+    (
+        (__version__, True),
+        ("0.0.1", True),
+        ("0.1.0", True),
+        ("1.0.0", True),
+        ("1.0.1", True),
+        ("1.1.1", True),
+        ("1.1.1-rc0", True),
+        ("1.1.1-rc.0", True),
+        ("1.1.1-rc-0", True),
+        ("1.0.1-dev0", True),
+        ("1.0.1-dev.0", True),
+        ("1.0.1-dev.1", True),
+        ("1.0.1-dev.2", True),
+        ("1.0.1-alpha.0", True),
+        ("1.0.1-alpha.266", True),
+        ("1.0.1-dev.0", True),
+        ("1.0.1-beta.0", True),
+        ("1.1.1-alpha.99999", True),
+        ("1.1.1-rc.99999", True),
+        ("1.1.99999", True),
+        ("1.999999.1", True),
+    ),
+)
+def test_version(entrance, expected):
+    assert re.fullmatch(r"\d\.\d\.\d(-\w+\.\d+)?", __version__, flags=re.I)
+
+
+@pytest.mark.parametrize(
     "entrance expected".split(),
     [
         (("0.5.11", "aaa"), "00000511.099999"),
