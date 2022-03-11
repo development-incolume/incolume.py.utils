03cda1397e45376cfee4b4987442ba3e8709ad8b
Britodfbr <britodfbr@gmail.com>
2022-03-11 14:52:35 -0300

    test(refactor): #41 Validação de nova implementação

--- tests/test_incolumepy_utils.py
+++ tests/test_incolumepy_utils.py
@@ -202,7 +202,7 @@ def test_apply_key_versions_2_sort(entrance, reverse, expected):
 
 
 @pytest.mark.parametrize(
-    'str_testing reverse expected'.split(),
+    "str_testing reverse expected".split(),
     [
         pytest.param(
             "0.1.0 system\n0.1.1 Fake record\n0.1.1-alpha.0 Fake record\n0.1.1-rc.0 Fake record"
@@ -210,7 +210,11 @@ def test_apply_key_versions_2_sort(entrance, reverse, expected):
             True,
             (
                 "# CHANGELOG\n"
-                "\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),"
+                " and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). "
+                "This file was automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n\n"
                 "---\n"
                 "- **0.2.0**: Fake record\n"
                 "- **0.1.1**: Fake record\n"
@@ -230,6 +234,12 @@ def test_apply_key_versions_2_sort(entrance, reverse, expected):
             False,
             (
                 "# CHANGELOG\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a "
+                "Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres "
+                "to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). This file was "
+                "automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n"
                 "\n"
                 "---\n"
                 "- **0.0.1**: Fake record\n"
@@ -241,75 +251,99 @@ def test_apply_key_versions_2_sort(entrance, reverse, expected):
                 "- **0.2.0**: Fake record\n"
                 "---\n"
                 "\n"
-            ),
+            )
             # marks=pytest.mark.skip
         ),
         pytest.param(
-            '',
+            "",
             False,
             (
                 "# CHANGELOG\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a "
+                "Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres "
+                "to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). This file was "
+                "automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n"
                 "\n"
                 "---\n"
                 "---\n"
                 "\n"
             ),
-            # marks=pytest.mark.skip
+            # marks=pytest.mark.skip,
         ),
         pytest.param(
-            '',
+            "",
             None,
             (
                 "# CHANGELOG\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a "
+                "Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres "
+                "to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). This file was "
+                "automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n"
                 "\n"
                 "---\n"
                 "---\n"
                 "\n"
             ),
-            # marks=pytest.mark.skip
+            # marks=pytest.mark.skip,
         ),
         pytest.param(
-            '0.0.1 initial\nwip fake record\nWIP nihil\nquisquiliae fake\n0.1.0-alpha.0 dev fake\nv0.1.0 record',
+            "0.0.1 initial\nwip fake record\nWIP nihil\nquisquiliae fake\n0.1.0-alpha.0 dev fake\nv0.1.0 record",
             True,
             (
                 "# CHANGELOG\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a "
+                "Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres "
+                "to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). This file was "
+                "automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n"
                 "\n"
                 "---\n"
-                '- **v0.1.0**: record\n'
-                '- **0.1.0-alpha.0**: dev fake\n'
-                '- **0.0.1**: initial\n'
+                "- **v0.1.0**: record\n"
+                "- **0.1.0-alpha.0**: dev fake\n"
+                "- **0.0.1**: initial\n"
                 "---\n"
                 "\n"
             ),
-            # marks=pytest.mark.skip
+            # marks=pytest.mark.skip,
         ),
         pytest.param(
-            'v0.0.1 initial\nv0.1.0 record\nv0.1.0-alpha.0 dev fake\nv0.1.0-alpha.1 ass\nv0.0.1-rc.0 as'
-            '\nv0.0.1-alpha.0 as\nv0.0.1-beta.0 a\nv0.0.1-dev.0 asd\nv0.0.1-rc.1 as\nv0.0.1-rc.2 as'
-            '\nv0.0.1-rc.11 as\nv0.0.1-rc.3 as\nv0.0.1-rc.12 as\nv0.0.1-rc.21 as\n',
+            "v0.0.1 initial\nv0.1.0 record\nv0.1.0-alpha.0 dev fake\nv0.1.0-alpha.1 ass\nv0.0.1-rc.0 as"
+            "\nv0.0.1-alpha.0 as\nv0.0.1-beta.0 a\nv0.0.1-dev.0 asd\nv0.0.1-rc.1 as\nv0.0.1-rc.2 as"
+            "\nv0.0.1-rc.11 as\nv0.0.1-rc.3 as\nv0.0.1-rc.12 as\nv0.0.1-rc.21 as\n",
             True,
             (
                 "# CHANGELOG\n"
+                "All notable changes to this project will be documented in this file.\n"
+                "The format is based on [Keep a "
+                "Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres "
+                "to [Semantic Versioning](https://semver.org/spec/v2.0.0.html). This file was "
+                "automatically generated for "
+                f"[{__version__}@incolumepy.utils](https://gitlab.com/development-incolume/incolumepy.utils))\n"
                 "\n"
                 "---\n"
-                '- **v0.1.0**: record\n'
-                '- **v0.1.0-alpha.1**: ass\n'
-                '- **v0.1.0-alpha.0**: dev fake\n'
-                '- **v0.0.1**: initial\n'
-                '- **v0.0.1-rc.21**: as\n'
-                '- **v0.0.1-rc.12**: as\n'
-                '- **v0.0.1-rc.11**: as\n'
-                '- **v0.0.1-rc.3**: as\n'
-                '- **v0.0.1-rc.2**: as\n'
-                '- **v0.0.1-rc.1**: as\n'
-                '- **v0.0.1-rc.0**: as\n'
-                '- **v0.0.1-alpha.0**: as\n'
-                '- **v0.0.1-beta.0**: a\n'
-                '- **v0.0.1-dev.0**: asd\n'
+                "- **v0.1.0**: record\n"
+                "- **v0.1.0-alpha.1**: ass\n"
+                "- **v0.1.0-alpha.0**: dev fake\n"
+                "- **v0.0.1**: initial\n"
+                "- **v0.0.1-rc.21**: as\n"
+                "- **v0.0.1-rc.12**: as\n"
+                "- **v0.0.1-rc.11**: as\n"
+                "- **v0.0.1-rc.3**: as\n"
+                "- **v0.0.1-rc.2**: as\n"
+                "- **v0.0.1-rc.1**: as\n"
+                "- **v0.0.1-rc.0**: as\n"
+                "- **v0.0.1-alpha.0**: as\n"
+                "- **v0.0.1-beta.0**: a\n"
+                "- **v0.0.1-dev.0**: asd\n"
                 "---\n"
                 "\n"
             ),
-            # marks=pytest.mark.skip
+            # marks=pytest.mark.skip,
         ),
     ],
 )
