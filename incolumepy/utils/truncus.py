836f0595a47a9b78b2887ddd6c6fc396f4d5ec26
britodfbr <britodfbr@gmail.com>
2022-12-07 15:38:46 -0300

    feat: #83 Tratativas para namespace

--- incolumepy/utils/truncus.py
+++ incolumepy/utils/truncus.py
@@ -41,17 +41,69 @@ def void2():
     return result
 
 
+def void3(package_name: str = None):
+    package_name = package_name or 'incolume.py.package.subpackage.module'
+    result = []
+    temp = ''
+    for bit in package_name.split('.')[:-1]:
+        temp = f'{temp}.{bit}' if temp else bit
+        print(temp)
+        result.append(temp)
+    return result
+
+
+def void4(package_name: str = None):
+    # package_name = package_name or 'incolume.py.package.subpackage.module'
+    result = []
+    temp = ''
+    bits = package_name.split('.')
+    if len(bits) <= 1:
+        return bits
+
+    for bit in bits[:-1]:
+        temp = f'{temp}.{bit}' if temp else bit
+        # print(temp)
+        result.append(temp)
+    return result
+
+
+def void5(package_name: str = None):
+    # package_name = package_name or 'incolume.py.package.subpackage.module'
+    result = []
+    temp = ''
+    try:
+        bits = package_name.split('.')
+    except AttributeError:
+        return result
+
+    if len(bits) <= 1:
+        return bits
+
+    for bit in bits[:-1]:
+        temp = f'{temp}.{bit}' if temp else bit
+        # print(temp)
+        result.append(temp)
+    return result
+
+
 def run():
     print(
-        # void0(),
-        # void1(),
-        void2(),
+
         s1 := 'incolume.py.package.module',
         nspace(s1),
         # s2 := 'incolume.module',
         # nspace(s2),
         # s3 := 'module',
         # nspace(s3),
+        '',
+        # void0(),
+        # void1(),
+        # void2(),
+        # void3(),
+        void5('module'),
+        void5('package.module'),
+        void5('incolume.py.package.subpackage.module'),
+        void5(None),
         '',
         sep='\n'
     )
