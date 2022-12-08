86f129c28bc7475b6dbe00be87d9c82eb60da583
britodfbr <britodfbr@gmail.com>
2022-12-08 14:56:50 -0300

    test(refactor): #69 Reescrita dos testes UnitTest em Pytest para incolumepy.utils.decorators.time_it.

--- tests/test_incolumepy_utils_decorators.py
+++ tests/test_incolumepy_utils_decorators.py
@@ -2,94 +2,134 @@
 
 import io
 import sys
-from unittest import TestCase, main
-from unittest.mock import patch
-
+import re
+import pytest
+from inspect import stack
 from incolumepy.utils.decorators import time_it
 
+# from unittest import TestCase, main
+# from unittest.mock import patch
 
-class DecoratorTests(TestCase):
-    """Class for decorator tests."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Class setup."""
+
+class TestCaseDecoratorTimeIt:
+    """Class test for decorator time_it."""
 
     @time_it
     def xpto(self):
         """Test for xpto."""
         n = 1
-        for i in range(100000):
+        for i in range(100_000):
             n *= i**2
         return n
 
     @time_it
     def func_x(self):
-        """Test always ok!."""
+        """Test for func_x."""
         return "func_x"
 
-    def setUp(self):
-        """Session setup."""
-        self.held, sys.stdout = sys.stdout, io.StringIO()
-
-    def test_name(self):
+    @pytest.mark.parametrize(
+        "entrance",
+        (
+            "xpto",
+            "func_x",
+        ),
+    )
+    def test_name(self, entrance):
         """Test name."""
-        self.assertEqual("xpto", self.xpto.__name__)
-        self.assertEqual("func_x", self.func_x.__name__)
+        assert entrance == getattr(self, entrance).__name__
 
-    def test_doc(self):
-        """Test doc."""
-        self.assertEqual("This ok!", self.xpto.__doc__)
-        self.assertEqual("This ok!", self.func_x.__doc__)
+    @pytest.mark.parametrize(
+        "entrance expected".split(),
+        (
+            ("xpto", "Test for xpto."),
+            ("func_x", "Test for func_x."),
+        ),
+    )
+    def test_docstring(self, entrance, expected):
+        """Test docstring."""
+        assert getattr(self, entrance).__doc__ == expected
 
-    def test_return(self):
+    @pytest.mark.parametrize(
+        "entrance expected".split(),
+        (
+            ("xpto", 0),
+            ("func_x", "func_x"),
+        ),
+    )
+    def test_return(self, entrance, expected):
         """Test return."""
-        self.assertEqual(0, self.xpto())
-        self.assertEqual("func_x", self.func_x())
+        assert getattr(self, entrance)() == expected
 
-    def test_output(self):
-        """Test output."""
-        self.xpto()
-        output = sys.stdout.getvalue().strip()
-        self.assertNotEqual("", output)
-
-    def test_output_mock0(self):
-        """Test output with mock."""
-        with patch("sys.stdout", new=io.StringIO()) as fakeoutput:
-            self.xpto()
-            self.assertRegex(
-                fakeoutput.getvalue().strip(), r"^xpto: \d*.?\d+ ms$"
-            )
-
-    def test_output_mock1(self):
-        """Test output with mock."""
-        with patch("sys.stdout", new=io.StringIO()) as fakeoutput:
-            self.func_x()
-            self.assertRegex(
-                fakeoutput.getvalue().strip(), r"^func_x: \d*.?\d+ ms$"
-            )
-
-    def test_output0(self):
-        """Test output."""
-        self.xpto()
-        self.assertRegex(sys.stdout.getvalue(), r"^xpto: \d*.?\d+ ms$")
-
-    def test_output1(self):
-        """Test output."""
-        this = self.xpto
-        this()
-        self.assertRegex(
-            sys.stdout.getvalue(), rf"^{this.__name__}: \d*.?\d+ ms$"
-        )
-
-    def test_output2(self):
-        """Test output."""
-        this = self.func_x
-        this()
-        self.assertRegex(
-            sys.stdout.getvalue(), rf"^{this.__name__}: \d*.?\d+ ms$"
-        )
+    @pytest.mark.parametrize(
+        'entrance',
+        (
+            'xpto',
+            'func_x',
+        ),
+    )
+    def test_output_timeit(self, entrance, capsys):
+        result = getattr(self, entrance)()
+        out, err = capsys.readouterr()
+        assert re.match(rf"^{entrance}: \d*.?\d+ ms$", out, re.I)
 
 
-if __name__ == "__main__":
-    main()
+
+# class DecoratorTests(TestCase):
+#     """Class for decorator tests."""
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         """Class setup."""
+#
+#     def setUp(self):
+#         """Session setup."""
+#         self.held, sys.stdout = sys.stdout, io.StringIO()
+#
+#
+#     def test_output(self):
+#         """Test output."""
+#         self.xpto()
+#         output = sys.stdout.getvalue().strip()
+#         self.assertNotEqual("", output)
+#
+#     def test_output_mock0(self):
+#         """Test output with mock."""
+#         with patch("sys.stdout", new=io.StringIO()) as fakeoutput:
+#             self.xpto()
+#             self.assertRegex(
+#                 fakeoutput.getvalue().strip(), r"^xpto: \d*.?\d+ ms$"
+#             )
+#
+#     def test_output_mock1(self):
+#         """Test output with mock."""
+#         with patch("sys.stdout", new=io.StringIO()) as fakeoutput:
+#             self.func_x()
+#             self.assertRegex(
+#                 fakeoutput.getvalue().strip(), r"^func_x: \d*.?\d+ ms$"
+#             )
+#
+#     def test_output0(self):
+#         """Test output."""
+#         self.xpto()
+#         self.assertRegex(sys.stdout.getvalue(), r"^xpto: \d*.?\d+ ms$")
+#
+#     def test_output1(self):
+#         """Test output."""
+#         this = self.xpto
+#         this()
+#         self.assertRegex(
+#             sys.stdout.getvalue(), rf"^{this.__name__}: \d*.?\d+ ms$"
+#         )
+#
+#     def test_output2(self):
+#         """Test output."""
+#         this = self.func_x
+#         this()
+#         self.assertRegex(
+#             sys.stdout.getvalue(), rf"^{this.__name__}: \d*.?\d+ ms$"
+#         )
+#
+#
+# if __name__ == "__main__":
+#     main()
