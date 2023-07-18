666b41a6f8abd0a56264470c3138a8c90a39d6f1
britodfbr <britodfbr@gmail.com>
2023-07-18 13:02:09 -0300

    style: #101 Aplicado correção indicada por linters

--- incolumepy/utils/cli.py
+++ incolumepy/utils/cli.py
@@ -1,13 +1,31 @@
+"""Command Line Interface module."""
+
+from pathlib import Path
+
 import click
+
 from incolumepy.utils.changelog import update_changelog
 
+
 @click.command()
-@click.argument('nome', envvar='USER', type=click.STRING)
+@click.argument("nome", envvar="USER", type=click.STRING)
 def greeting(nome):
-    click.echo(f'Oi {nome}!')
+    """Retorna o cumprimento para o nome passado.
+
+    :param nome: str
+    :return: None
+    """
+    click.echo(f"Oi {nome}!")
+
 
 @click.command()
 # @click.argument('stream', type=click.STRING)
-@click.argument('file_changelog', type=click.STRING, default='CHANGELOG.md')
-def changelog(file_changelog):
+@click.argument("file_changelog", type=click.STRING, default="CHANGELOG.md")
+def changelog(file_changelog: str | Path):
+    """Operacionaliza uma interface CLI para módulo incolumepy.utils.changelog.
 
+    :param changelog_file:  changelog full filename.
+    :return: bool. True if success
+
+    """
+    return update_changelog(changelog_file=file_changelog)
