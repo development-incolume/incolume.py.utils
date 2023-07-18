5ddb61f2a09fe644a203c28173d5bd634e6c000a
britodfbr <britodfbr@gmail.com>
2023-07-18 11:58:25 -0300

    fix: #101 Correção na chamada do módulo incolumepy.utils.changelog

--- incolumepy/utils/cli.py
+++ incolumepy/utils/cli.py
@@ -1,10 +1,13 @@
 import click
-from incolumepy.utils import changelog
+from incolumepy.utils.changelog import update_changelog
 
 @click.command()
 @click.argument('nome', envvar='USER', type=click.STRING)
-def changelog(nome):
+def greeting(nome):
     click.echo(f'Oi {nome}!')
 
+@click.command()
+# @click.argument('stream', type=click.STRING)
+@click.argument('file_changelog', type=click.STRING, default='CHANGELOG.md')
+def changelog(file_changelog):
 
-changelog()
