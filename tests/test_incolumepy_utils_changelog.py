31b806d74abb8fef087c259fa6080490f0a2076e
britodfbr <britodfbr@gmail.com>
2023-07-19 09:45:33 -0300

    Bumping version from 2.7.1 to 2.8.0a0

--- tests/test_incolumepy_utils_changelog.py
+++ tests/test_incolumepy_utils_changelog.py
@@ -188,8 +188,8 @@ class TestClassChangelog:
         "entrance",
         (
             {},
-            {'reverse': False},
-        )
+            {"reverse": False},
+        ),
     )
     def test_init(self, entrance):
         """Test for init class."""
@@ -201,33 +201,47 @@ class TestClassChangelog:
         (
             (
                 {},
-                ['# CHANGELOG\n\n\n', 'All notable changes to this project',
-                 ' will be documented in this file.\n\n',
-                 'The format is based on ',
-                 '[Keep a Changelog](https://keepachangelog.com/en/1.0.0/), ',
-                 'this project adheres to [Semantic Versioning]'
-                 '(https://semver.org/spec/v2.0.0.html) and '
-                 '[Conventional Commit](https://www.conventionalcommits.org/'
-                 'pt-br/v1.0.0/).\n\n',
-                 'This file was automatically generated for',
-                 ' [incolumepy.utils](https://gitlab.com/development-incolume/'
-                 'incolumepy.utils/-/tree/2.7.1)', '\n\n---\n']
+                [
+                    "# CHANGELOG\n\n\n",
+                    "All notable changes to this project",
+                    " will be documented in this file.\n\n",
+                    "The format is based on ",
+                    "[Keep a Changelog]"
+                    "(https://keepachangelog.com/en/1.0.0/), ",
+                    "this project adheres to [Semantic Versioning]"
+                    "(https://semver.org/spec/v2.0.0.html) and "
+                    "[Conventional Commit]"
+                    "(https://www.conventionalcommits.org/"
+                    "pt-br/v1.0.0/).\n\n",
+                    "This file was automatically generated for",
+                    " [incolumepy.utils]"
+                    "(https://gitlab.com/development-incolume/"
+                    "incolumepy.utils/-/tree/2.7.1)",
+                    "\n\n---\n",
+                ],
             ),
             (
-                {'reverse': False},
-                ['# CHANGELOG\n\n\n', 'All notable changes to this project',
-                 ' will be documented in this file.\n\n',
-                 'The format is based on ',
-                 '[Keep a Changelog](https://keepachangelog.com/en/1.0.0/), ',
-                 'this project adheres to [Semantic Versioning]'
-                 '(https://semver.org/spec/v2.0.0.html) and '
-                 '[Conventional Commit](https://www.conventionalcommits.org/'
-                 'pt-br/v1.0.0/).\n\n',
-                 'This file was automatically generated for',
-                 ' [incolumepy.utils](https://gitlab.com/development-incolume/'
-                 'incolumepy.utils/-/tree/2.7.1)', '\n\n---\n']
+                {"reverse": False},
+                [
+                    "# CHANGELOG\n\n\n",
+                    "All notable changes to this project",
+                    " will be documented in this file.\n\n",
+                    "The format is based on ",
+                    "[Keep a Changelog]"
+                    "(https://keepachangelog.com/en/1.0.0/), ",
+                    "this project adheres to [Semantic Versioning]"
+                    "(https://semver.org/spec/v2.0.0.html) and "
+                    "[Conventional Commit]"
+                    "(https://www.conventionalcommits.org/"
+                    "pt-br/v1.0.0/).\n\n",
+                    "This file was automatically generated for",
+                    " [incolumepy.utils]"
+                    "(https://gitlab.com/development-incolume/"
+                    "incolumepy.utils/-/tree/2.7.1)",
+                    "\n\n---\n",
+                ],
             ),
-        )
+        ),
     )
     def test_header(self, entrance, expected):
         """Test for header file."""
