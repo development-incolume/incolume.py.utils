67b5ed8a6cad10e42b81e2eb912673046424c6e2
Britodfbr <britodfbr@gmail.com>
2023-09-04 18:49:40 -0300

    test: #110 implementação de testes para os módulos changelog e cli
    Ampliação da cobertura de testes

--- tests/test_incolumepy_utils_cli.py
+++ tests/test_incolumepy_utils_cli.py
@@ -1,22 +1,48 @@
 """Command Line Interface - CLI module."""
-
 from pathlib import Path
 from tempfile import gettempdir
 
 import pytest
+from click.testing import CliRunner
 
 from incolumepy.utils.cli import changelog, greeting
 
 
 class TestCLI:
-    @pytest.mark.skip
-    def test_output(
-        self,
-    ):
-        file = Path(gettempdir()) / "CHANGELOG.md"
-        changelog(file_changelog=file)
-        assert True
+    # @pytest.mark.skip
+    def test_greeting(self, capsys):
+        runner = CliRunner()
+        result = runner.invoke(greeting, ["Peter"])
+        assert result.exit_code == 0
+        assert result.output == "Oi Peter!\n"
 
-    @pytest.mark.skip
-    def test_greeting(self):
-        greeting("ops")
+    @pytest.mark.parametrize(
+        "entrance",
+        (
+            "# CHANGELOG",
+            "[Keep a Changelog]",
+            "[Semantic Versioning]",
+            "[Conventional Commit]",
+            "[incolumepy.utils]",
+        ),
+    )
+    def test_output(self, entrance):
+        runner = CliRunner()
+        file = Path(gettempdir()) / "CHANGELOG.md"
+        fakeurl = "http://fake.incolume.com.br/xpto"
+        result = runner.invoke(
+            changelog,
+            [
+                file.as_posix(),
+                "-p",
+                "false",
+                "-u",
+                fakeurl,
+                "-r",
+                "true",
+            ],
+        )
+        assert result
+        content = file.read_text()
+        assert fakeurl in content
+        assert entrance in content
