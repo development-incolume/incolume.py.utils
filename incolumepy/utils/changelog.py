b15389ffe8754a2f8638c99a91bed6dce1bbcdf4
Britodfbr <britodfbr@gmail.com>
2023-09-28 12:10:15 -0300

    fix: #115 tratativa 4 em corrigir erro de execução no windows, durante captura de data do commit.

--- incolumepy/utils/changelog.py
+++ incolumepy/utils/changelog.py
@@ -67,7 +67,7 @@ def msg_classify(msg: str, lang: str = "", **kwargs) -> Dict[str, Any]:
     key, msg = msg.split(maxsplit=1)
 
     date = subprocess.getoutput(
-        'git show -s --pretty="%%cs" %s^{commit}'
+        'git show -s --pretty="%%cs" %s^{commit} --'
         % key  # pylint: disable=C0209
     )
     logging.debug("key=%s; date=%s; msg=%s", key, date, msg)
