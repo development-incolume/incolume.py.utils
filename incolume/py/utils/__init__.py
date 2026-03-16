a83fb0f40d6a793ecdb8d936ea6412e21f1fee7d
Britodfbr <britodfbr@gmail.com>
2026-03-16 18:23:52 -0300

    refactor: Formatação Pythonica

--- incolume/py/utils/__init__.py
+++ incolume/py/utils/__init__.py
@@ -13,7 +13,7 @@ confproject = Path(__file__).parents[3] / 'pyproject.toml'
 versionfile = Path(__file__).parent / 'version.txt'
 
 with suppress(FileNotFoundError), confproject.open('rb') as f:
-    versionfile.write_text(tomli.load(f)['project']['version'] + '\n')
+    versionfile.write_text(f'{tomli.load(f)["project"]["version"]}\n')
 
 __version__ = versionfile.read_text().strip()
 __title__ = 'incolume.py.utils'
